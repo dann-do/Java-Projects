87479d98ea71bb966916156cc2e68b3601ce6e91
dann-do <danpndo@gmail.com>
2024-01-25 16:22:57 -0800

    Multiple files deleted

--- TestAnimal.java
+++
@@ -1,9 +0,0 @@
-public class TestAnimal{
-	public static void main(String[] args){
-		Animal[] zoo = { new Dog(), new Fish(), new Bird() };
-		for (Animal animal : zoo){
-			animal.move();
-			System.out.println( animal.call() );
-		}
-	}
-}
